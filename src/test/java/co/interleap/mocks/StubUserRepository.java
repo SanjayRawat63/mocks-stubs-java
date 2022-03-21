c3a3b9199c19fe428189ae28dd521c3c26b71201
Sanjay Rawat <63999697+SanjayRawat63@users.noreply.github.com>
2022-03-21 10:43:58 +0530

    Assignment 3 Stub with exceptions

--- src/test/java/co/interleap/mocks/StubUserRepository.java
+++ src/test/java/co/interleap/mocks/StubUserRepository.java
@@ -4,7 +4,7 @@ public class StubUserRepository implements UserRepository {
 
     @Override
     public User findByEmail(String email) throws NotFoundException {
-        if ("world@gmail.com".equals(email))
+        if (!"hello@gmail.com".equals(email))
             throw new NotFoundException();
         return new User("9373940583", "hello@gmail.com", "sanju");
     }
