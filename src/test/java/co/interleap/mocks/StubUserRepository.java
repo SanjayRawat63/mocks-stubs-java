a2a8a1f345fc99c9272134540fa8d5069898f30b
Sanjay Rawat <63999697+SanjayRawat63@users.noreply.github.com>
2022-03-21 10:29:01 +0530

    Stub with exceptions

--- src/test/java/co/interleap/mocks/StubUserRepository.java
+++ src/test/java/co/interleap/mocks/StubUserRepository.java
@@ -4,6 +4,8 @@ public class StubUserRepository implements UserRepository {
 
     @Override
     public User findByEmail(String email) throws NotFoundException {
-        return new User("9373940583","hello@gmail.com","sanju");
+        if ("world@gmail.com".equals(email))
+            throw new NotFoundException();
+        return new User("9373940583", "hello@gmail.com", "sanju");
     }
 }
--- src/test/java/co/interleap/mocks/UserServiceTest.java
+++ src/test/java/co/interleap/mocks/UserServiceTest.java
@@ -2,34 +2,42 @@ package co.interleap.mocks;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 class UserServiceTest {
 
     @Test
     void shouldSendWelcomeEmail() {
-        MockEmailService emailService=new MockEmailService();
-        UserService userService = new UserService(null,emailService);
+        MockEmailService emailService = new MockEmailService();
+        UserService userService = new UserService(null, emailService);
         userService.sendWelcomeEmail("hello@gmail.com");
-        EmailBody expectedEmail = new EmailBody("Welcome","Welcome to the portal","hello@gmail.com");
+        EmailBody expectedEmail = new EmailBody("Welcome", "Welcome to the portal", "hello@gmail.com");
         emailService.verify(expectedEmail);
     }
 
     @Test
-    void shouldSendRegisteredPhoneNumberIfUserAccountExists() {
-        MockEmailService emailService=new MockEmailService();
-        StubUserRepository userRepository=new StubUserRepository();
-        UserService userService = new UserService(userRepository,emailService);
+    void shouldSendRegisteredPhoneNumberIfUserAccountExists() throws NotFoundException {
+        MockEmailService emailService = new MockEmailService();
+        StubUserRepository userRepository = new StubUserRepository();
+        UserService userService = new UserService(userRepository, emailService);
         userService.sendRegisteredPhoneNumber("hello@gmail.com");
         EmailBody expectedEmail = new EmailBody("Account Details",
                 "Here is your Registered Phone Number: \n" + "9373940583",
                 "hello@gmail.com");
         emailService.verify(expectedEmail);
 
+
     }
 
     @Test
-    void sendAccountNotFoundEmailForUnregisteredUsersWhenTryingToGetRegisteredPhoneNumber() {
-
+    void sendAccountNotFoundEmailForUnregisteredUsersWhenTryingToGetRegisteredPhoneNumber() throws NotFoundException {
+        MockEmailService emailService = new MockEmailService();
+        StubUserRepository userRepository = new StubUserRepository();
+        UserService userService = new UserService(userRepository, emailService);
+        userService.sendRegisteredPhoneNumber("world@gmail.com");
+        EmailBody expectedEmail = new EmailBody("Account Not Found",
+                "We do not have a registered account matching your email address",
+                "world@gmail.com");
+        emailService.verify(expectedEmail);
     }
 }
